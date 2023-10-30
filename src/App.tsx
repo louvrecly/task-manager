1d00f2b7f485f3e539fe9a5ce74f650da7f45e91
Louvre Chow <louvrecly@gmail.com>
2023-10-30 18:02:52 -0400

    store tasks in localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,19 @@
-import { useCallback, useMemo, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import NavBar from './components/NavBar';
 import TasksList from './components/TasksList';
 import Drawer from './components/Drawer';
 import TaskForm from './components/TaskForm';
-import { Task } from './types/task';
-
-const DUMMY_TASKS: Task[] = [
-  {
-    id: 1,
-    title: 'Learn React',
-    category: 'Personal',
-    dueDate: new Date('2023-11-15'),
-  },
-  {
-    id: 2,
-    title: 'Learn Python',
-    category: 'Personal',
-    dueDate: new Date('2023-11-30'),
-  },
-  {
-    id: 3,
-    title: 'Create Node.js server',
-    category: 'Work',
-    dueDate: new Date('2023-12-15'),
-  },
-  {
-    id: 4,
-    title: 'Connect App to Firebase',
-    category: 'School',
-    dueDate: new Date('2023-12-20'),
-  },
-  {
-    id: 5,
-    title: 'Learn SQL',
-    category: 'School',
-    dueDate: new Date('2023-11-21'),
-  },
-  {
-    id: 6,
-    title: 'Learn MongoDB',
-    category: 'School',
-    dueDate: new Date('2023-12-10'),
-  },
-  {
-    id: 7,
-    title: 'Deploy App to AWS',
-    category: 'Work',
-    dueDate: new Date('2023-10-21'),
-  },
-  {
-    id: 8,
-    title: 'Learn Flutter',
-    category: 'Personal',
-    dueDate: new Date('2023-10-02'),
-  },
-];
+import { Task, convertFormValuesToTask } from './types/task';
 
 const App = () => {
+  const tasksString = localStorage.getItem('tasks');
+
+  const initialTasks: Task[] = JSON.parse(tasksString ?? '[]').map(
+    convertFormValuesToTask,
+  );
+
   const [showForm, setShowForm] = useState(false);
-  const [tasks, setTasks] = useState(DUMMY_TASKS);
+  const [tasks, setTasks] = useState(initialTasks);
 
   const maxId = useMemo(() => tasks.length, [tasks]);
   const sortedTasks = useMemo(
@@ -73,6 +28,10 @@ const App = () => {
     setTasks((existingTasks) => [...existingTasks, task]);
     setShowForm(false);
   }, []);
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(sortedTasks));
+  }, [sortedTasks]);
 
   return (
     <div>
